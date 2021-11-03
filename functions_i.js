4389609367cc4a92aedaa67615e55341f0b4381c
= <=>
2021-11-03 20:54:01 +0100

    functions

---
+++ functions_i.js
@@ -0,0 +1,9 @@
+r=1
+function add(a,b){
+   return a+b; 
+}
+
+s=add(5,6)
+console.assert(add(5,6)===11,':(');
+console.log(s)
+console.log(typeof(add));
---
+++ object.js
@@ -0,0 +1,17 @@
+compte411 = {
+    code: 411,
+    label: 'Client',
+    totalDebit:0,
+    totalCredit:0,
+    soled:0,
+}
+
+
+console.dir(compte411)
+
+console.log(compte411['code']);
+console.log(compte411.code)
+
+for (props in compte411){
+    console.log(props)
+}
